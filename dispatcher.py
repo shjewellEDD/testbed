30deda48f2e63c6321d2c8fb2408df0e30e06639
shjewellEDD <scott.jewell@noaa.gov>
2022-03-17 14:43:45 -0700

    Added master_app import and call in dispatcher.py

--- dispatcher.py
+++ dispatcher.py
@@ -2,9 +2,10 @@ import m200_dash as m200
 import telonas2_dash as telo2
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 import flask
-import flask_app
+#import flask_app
+import master_app
 
-app = DispatcherMiddleware({
+app = DispatcherMiddleware(master_app, {
     '/prawler/m200': m200.server,
     '/prawler/telonas2': telo2.server
 })
