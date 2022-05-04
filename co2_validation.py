7c923b0e71fe6c9209f7c78fab005b16b8a2347c
shjewellEDD <scott.jewell@noaa.gov>
2022-05-04 16:42:27 -0700

    First five plots complete (now with server)

--- co2_validation.py
+++ co2_validation.py
@@ -44,9 +44,9 @@ colors = {'Dark': '#111111', 'Light': '#443633', 'text': '#7FDBFF'}
 
 app = dash.Dash(__name__,
                 meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
-#                requests_pathname_prefix='/co2/validation/',
+                requests_pathname_prefix='/co2/validation/',
                 external_stylesheets=[dbc.themes.SLATE])
-#server = app.server
+server = app.server
 
 filter_card = dbc.Card(
     dbc.CardBody(
